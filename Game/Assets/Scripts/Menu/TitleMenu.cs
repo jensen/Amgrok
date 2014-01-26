7ab39e2a6b395e91ce1d2f9e72146bec39bb03cb
Karl Jensen <karljensen@gmail.com>
2014-01-25 19:45:04 -0800

    Adding the title screen image. Move buttons down so they aren't blocking the title.

--- Game/Assets/Scripts/Menu/TitleMenu.cs
+++ Game/Assets/Scripts/Menu/TitleMenu.cs
@@ -2,15 +2,17 @@
 
 public class TitleMenu : Menu {
 
+	private float offsetFromCenter = 60;
+
 	void OnGUI() {
 		GUI.skin = skin;
 
-		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y, buttonWidth, buttonHeight), "Play"))
+		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y + offsetFromCenter, buttonWidth, buttonHeight), "Play"))
 		{
 			Application.LoadLevel("Main");
 		}
 
-		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y + buttonHeight + buttonPadding, buttonWidth, buttonHeight), "Quit"))
+		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y + offsetFromCenter + (buttonHeight + buttonPadding), buttonWidth, buttonHeight), "Quit"))
 		{
 			Application.Quit();
 		}
