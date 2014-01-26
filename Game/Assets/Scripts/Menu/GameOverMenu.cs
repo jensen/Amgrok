96a915cd5928104a1c9379f41e5444028336221c
Karl Jensen <karljensen@gmail.com>
2014-01-25 23:05:13 -0800

    Moving game over buttons to the center vertically. Sweet.

--- Game/Assets/Scripts/Menu/GameOverMenu.cs
+++ Game/Assets/Scripts/Menu/GameOverMenu.cs
@@ -6,13 +6,12 @@ public class GameOverMenu : Menu {
 	void OnGUI() {
 		GUI.skin = skin;
 
-		
-		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y, buttonWidth, buttonHeight), "Restart"))
+		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y - buttonHeight, buttonWidth, buttonHeight), "Restart"))
 		{
 			Application.LoadLevel("Main");
 		}
 		
-		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y + buttonHeight + buttonPadding, buttonWidth, buttonHeight), "Quit"))
+		if(GUI.Button (new Rect(screenCenter.x - buttonWidth/2, screenCenter.y + buttonHeight, buttonWidth, buttonHeight), "Quit"))
 		{
 			Application.LoadLevel("Title");
 		}
