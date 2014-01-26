2f676140f74f7c0f8a00eef3d740b19525a482f5
Deshin Finlay <finlay.deshin@gmail.com>
2014-01-25 22:21:14 -0800

    Homing Missiles now rotate you.

--- Game/Assets/Scripts/Enemies/HomingMissile.cs
+++ Game/Assets/Scripts/Enemies/HomingMissile.cs
@@ -11,6 +11,26 @@ public class HomingMissile : MonoBehaviour {
 		_enemy = GetComponent<Enemy>();
 	}
 	
+	void OnDestroyed() {
+		if (LevelManager.Instance.player != null) {
+			if (LevelManager.Instance.player.shootingDirection == 3) {
+				LevelManager.Instance.player.shootingDirection = 0;
+			} else {
+				LevelManager.Instance.player.shootingDirection += 1;
+			}
+		}
+	}
+
+	void OnTouchPlayer() {
+		if (LevelManager.Instance.player != null) {
+			if (LevelManager.Instance.player.shootingDirection == 0) {
+				LevelManager.Instance.player.shootingDirection = 3;
+			} else {
+				LevelManager.Instance.player.shootingDirection -= 1;
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (LevelManager.Instance.player != null) {
