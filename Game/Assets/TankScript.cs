555b217733f8cfc5f73a731e114f1289f2d4925b
Deshin Finlay <finlay.deshin@gmail.com>
2014-01-25 22:13:14 -0800

    Tanks finished (inc healing on kill)

--- Game/Assets/TankScript.cs
+++ Game/Assets/TankScript.cs
@@ -6,12 +6,18 @@ public class TankScript : MonoBehaviour {
 		private Enemy _enemy;
 		
 		public float speed = 0.5F;
-		
+		public float healAmount = 15F;
 		void Start() {
 			_enemy = GetComponent<Enemy>();
 			_enemy.speed = new Vector3 (0F, -speed, 0F);
 		}
-		
+
+		void OnDestroyed() {
+		if (LevelManager.Instance.player != null) { 
+			LevelManager.Instance.player.AddHealth(healAmount);
+		}
+	}
+
 		// Update is called once per frame
 		void Update () {
 		}
