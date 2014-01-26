e9f48df54a322f4a5d055b1c33a7564dc80eba65
Deshin Finlay <finlay.deshin@gmail.com>
2014-01-26 10:24:55 -0800

    Added pretitle.

---
+++ Game/Assets/FirstScreenScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstScreenScript : MonoBehaviour {
+
+	public float timeToSwitch = 1F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeToSwitch -= Time.deltaTime;
+		if (timeToSwitch <= 0) {
+			Application.LoadLevel("Title");
+				}
+	}
+}
