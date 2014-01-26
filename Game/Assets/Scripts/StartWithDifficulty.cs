bcde9cec2c4bc397f7da916651762ed11688a31d
Ming <project.shinkirou@gmail.com>
2014-01-26 10:42:26 -0800

    difficulty is now working

---
+++ Game/Assets/Scripts/StartWithDifficulty.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartWithDifficulty : MonoBehaviour {
+
+	public int difficulty = 0;
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds(.1f);
+		LevelManager.Instance.ChangeDifficulty(difficulty);
+		Destroy(gameObject);
+	}
+}
